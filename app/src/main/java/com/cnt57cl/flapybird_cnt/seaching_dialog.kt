e018e5d70a021bfeb50482d4c411cbd4bcefcfb5
son dang <dangson28121998@gmail.com>
2019-11-28 22:01:52 +0700

    this app is online game

--- app/src/main/java/com/cnt57cl/flapybird_cnt/seaching_dialog.kt
+++ app/src/main/java/com/cnt57cl/flapybird_cnt/seaching_dialog.kt
@@ -1,4 +1,29 @@
 package com.cnt57cl.flapybird_cnt
 
-class seaching_dialog {
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.View
+import android.view.Window
+
+class seaching_dialog(context: Context,view: View?):Dialog(context) {
+
+    var con:Context?=null
+    var view:View?=null
+    init {
+
+        this.con=context
+        this.view=view
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window?.decorView?.background=ColorDrawable(Color.TRANSPARENT)
+
+        setContentView(view!!)
+    }
+
 }
